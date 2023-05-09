32cca55dcd9c547807f9b6dd8ac0c3f270cebf68
Brooke Finister <brookeafinister@gmail.com>
2023-05-08 22:17:27 -0700

    [ADD] login route

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
-
+import authRoutes from "./routes/auth.js";
+import { register } from "./controllers/auth.js";
 
 /* CONFIGURATIONS */
 const __filename = fileURLToPath(import.meta.url);
@@ -34,3 +35,16 @@ const storage = multer.diskStorage({
   },
 });
 const upload = multer({ storage });
+
+// ROUTES WITH FILES
+app.post("/auth/register", upload.single("picture"), register);
+
+// ROUTES 
+app.use("/auth/", authRoutes)
+
+// MONGOOSE SETUP
+const PORT = process.env.PORT || 6001;
+mongoose
+    .connect(process.env.MONGO_URL, {
+
+    })
