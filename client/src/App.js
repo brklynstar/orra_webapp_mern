bf586376e080f6680d20cc2e2660a48fce80b286
Brooke Finister <brookeafinister@gmail.com>
2023-05-09 17:16:41 -0700

    [ADD] scenes with placeholders

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,18 @@
-import './App.css';
-
+import { BrowserRouter, Navigate, Routes, Route } from 'react-router-dom';
+import HomePage from 'scenes/homePage';
+import LoginPagae from 'scenes/loginPage';
+import ProfilePage from 'scenes/ProfilePage';
 function App() {
   return (
     <div className="app">
-  
+      <BrowserRouter>
+      <Routes>
+        <Route path ="/" element={<LoginPage />} />
+        <Route path ="/home" element ={<HomePage />} />
+        <Route path ="/profile/:userId" element  ={<ProfilePage />} />
+
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
