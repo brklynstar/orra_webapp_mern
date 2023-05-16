543e16862feb16b7f2fb7214f25e172639b37473
Brooke Finister <brookeafinister@gmail.com>
2023-05-15 23:36:10 -0700

    [WORKING] login page and register form fields, buttons and validation

--- client/src/scenes/loginPage/loginPage.jsx
+++ client/src/scenes/loginPage/loginPage.jsx
@@ -13,7 +13,7 @@ const LoginPage = () => {
         textAlign="center"
       >
         <Typography fontWeight="bold" fontSize="32px" color="primary">
-          Sociopedia
+          O.R.R.A
         </Typography>
       </Box>
 
@@ -25,7 +25,7 @@ const LoginPage = () => {
         backgroundColor={theme.palette.background.alt}
       >
         <Typography fontWeight="500" variant="h5" sx={{ mb: "1.5rem" }}>
-          Welcome to Socipedia, the Social Media for Sociopaths!
+          Welcome to O.R.R.A | Off-Road Recovery App
         </Typography>
       </Box>
     </Box>
